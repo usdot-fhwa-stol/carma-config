e459d646af2c1c02266316c5ba220f8749ef4471
Anish_deva <51463994+adev4a@users.noreply.github.com>
2024-05-31 12:56:49 -0400

    update config to match vru (#347)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    Updates the xil_cloud_telematics config with ail_vru changes in order to
    be able to run test cases from carma-system-4.5.0 release.
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    https://github.com/usdot-fhwa-stol/cda-telematics/issues/207
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [x] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x] I have read the **CONTRIBUTING** document.
    [CARMA Contributing
    Guide](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

--- xil_cloud_telematics/system_release.sh
+++ xil_cloud_telematics/system_release.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-#  Copyright (C) 2018-2021 LEIDOS.
-# 
+#  Copyright (C) 2018-2024 LEIDOS.
+#
 #  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 #  use this file except in compliance with the License. You may obtain a copy of
 #  the License at
-# 
+#
 #  http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 #  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #  License for the specific language governing permissions and limitations under
 #  the License.
 
-# This script takes a system release name and version number as arguments, and 
+# This script takes a system release name and version number as arguments, and
 # updates version dependencies in docker-compose.yml and docker-compose-background.yml
 # accordingly.
 
