0d0190f35098b9234a0cc6a0b81e9719ef4dcfd6
Misheel Bayartsengel <misheel.bayart@gmail.com>
2024-10-11 15:08:44 -0400

    update to year 2024

--- freightliner_cascadia_2012_dot_10002/carma_docker.launch.py
+++ freightliner_cascadia_2012_dot_10002/carma_docker.launch.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2022 LEIDOS.
+# Copyright (C) 2021-2024 Leidos.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
