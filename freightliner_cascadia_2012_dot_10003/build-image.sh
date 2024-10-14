15bf3be6225fe56fda01ff0c49069082a48458cf
Misheel Bayartsengel <misheel.bayart@gmail.com>
2024-10-14 09:09:13 -0400

    update example folder to support install guide (#391)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    This PR fixes multiple issues related to having carma start out of the
    box following the CARMA install guide.
    - Updated example_calibration_folder to include trajectory_follower
    folder so that carma doesn't shut down automatically not being able to
    find the file.
    - Removed the annoying `git checkout docker-compose.yml
    docker-compose-background.yml` commands that overwrites any developer
    changes locally. This prompted little factorizaton to reduce code
    duplication if separate if clauses.
    - Fixed a syntax error introduced in this PR recently:
    https://github.com/usdot-fhwa-stol/carma-config/pull/388
    
    This also supports opt/carma folder setup script here:
    https://github.com/usdot-fhwa-stol/carma-platform/pull/2449
    
    NOTE: This repo does need some improvements in general which I didn't
    address here:
    - build-image.sh script (possibly more) is largely the same in all
    folders and may need code refactoring to reduce duplication
    - not sure if `-d` option is really useful or operational anymore after
    we introduced `.env` to handle docker org and tags.
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    https://usdot-carma.atlassian.net/browse/CXC-94
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    I am writing CDASim tutorial guide. And its configuration step required
    some changes to our installation guides, which I discovered these
    issues.
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    local integration tested by calling scripts in
    https://github.com/usdot-fhwa-stol/carma-platform/pull/2449
    and building images locally
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [X] Defect fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [X] My change requires a change to the documentation.
    - [X] I have updated the documentation accordingly.
    - [X] I have read the **CONTRIBUTING** document.
    [CARMA Contributing
    Guide](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    - [X] I have added tests to cover my changes.
    - [X] All new and existing tests passed.

--- freightliner_cascadia_2012_dot_10003/build-image.sh
+++ freightliner_cascadia_2012_dot_10003/build-image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #  Copyright (C) 2018-2021 LEIDOS.
-# 
+#
 #  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 #  use this file except in compliance with the License. You may obtain a copy of
 #  the License at
-# 
+#
 #  http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 #  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -54,25 +54,17 @@ echo "Building docker image for CARMA Configuration version: $TAG"
 echo "Final image name: $USERNAME/$IMAGE:$TAG"
 
 if [[ $TAG = "develop-$CONFIG_NAME" ]]; then
-    git checkout -- docker-compose.yml
     sed -i "s|usdotfhwastoldev/|$USERNAME/|g; s|usdotfhwastol/|$USERNAME/|g; s|:[0-9]*\.[0-9]*\.[0-9]*|:develop|g; s|:CARMASystem_[0-9]*\.[0-9]*\.[0-9]*|:develop|g;" \
         docker-compose.yml
     sed -i "s|usdotfhwastoldev/|$USERNAME/|g; s|usdotfhwastol/|$USERNAME/|g; s|:[0-9]*\.[0-9]*\.[0-9]*|:develop|g; s|:CARMASystem_[0-9]*\.[0-9]*\.[0-9]*|:develop|g;" \
         docker-compose-background.yml
-    docker build --no-cache -t $USERNAME/$IMAGE:$TAG \
-    --build-arg VERSION="$TAG" \
-    --build-arg VCS_REF=`git rev-parse --short HEAD` \
-    --build-arg CONFIG_NAME="carma-config:$CONFIG_NAME" \
-    --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` .
-    git checkout -- docker-compose.yml docker-compose-background.yml
-else
-    git checkout -- docker-compose.yml
-    docker build --no-cache -t $USERNAME/$IMAGE:$TAG \
-    --build-arg VERSION="$TAG" \
-    --build-arg VCS_REF=`git rev-parse --short HEAD` \
-    --build-arg CONFIG_NAME="carma-config:$CONFIG_NAME" \
-    --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` .
 fi
+
+docker build --no-cache -t $USERNAME/$IMAGE:$TAG \
+    --build-arg VERSION="$TAG" \
+    --build-arg VCS_REF=`git rev-parse --short HEAD` \
+    --build-arg CONFIG_NAME="carma-config:$CONFIG_NAME" \
+    --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` .
 
 echo ""
 echo "##### CARMA $CONFIG_NAME Docker Image Build Done! #####"
