581592bd04f78b6505a11c695bfcfec6ad2c9df4
Mahbubul Alam Palash <palashm@leidos.com>
2025-02-28 10:22:26 -0500

    TT-160 Added commnets on how to change values in OD file to change number of vehicles in horizontal and vertical direction

--- xil-Town05/cdasim_config/sumo_background_traffic/generate_route.sh
+++ xil-Town05/cdasim_config/sumo_background_traffic/generate_route.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#This script  utilizes sumo od2trips ( https://sumo.dlr.de/docs/od2trips.html ) tool to generate background traffic for Town05 given Traffic assignment zones and OD matrix
+#This script  utilizes sumo od2trips ( https://sumo.dlr.de/docs/od2trips.html ) tool to generate background traffic for Town05 given Traffic assignment zones (TAZ) and OD matrix
+
+#To change the number of vehicles in the horizontal or vertical direction, modify the values in the third column of OD_file.od, which specify the number of vehicles, and then re-run the generate_route.sh script to generate the route file with the updated vehicle count. The first two columns in the OD matrix specify the origin and destination, while the third column defines the number of vehicles traveling between them. For a detailed guideline, refer to SUMO od2trips documentation here  https://sumo.dlr.de/docs/od2trips.html
+
 #This script requires Sumo tools od2trips and duarouter and sumo tools can installed  using the command:  sudo apt-get install sumo-tools (https://sumo.dlr.de/docs/Installing/index.html)
 
 od2trips -c od2trips.config.xml -n Taz.xml --od-matrix-files OD_file.od -o od_file.odtrips.xml
