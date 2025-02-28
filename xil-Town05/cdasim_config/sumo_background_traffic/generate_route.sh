cdf60f5f3b0c0543e146312801b871b63a00d5d9
Mahbubul Alam Palash <palashm@leidos.com>
2025-02-28 09:53:06 -0500

    TT-160 Addressing PR comments

--- xil-Town05/cdasim_config/sumo_background_traffic/generate_route.sh
+++ xil-Town05/cdasim_config/sumo_background_traffic/generate_route.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+
+#This script  utilizes sumo od2trips ( https://sumo.dlr.de/docs/od2trips.html ) tool to generate background traffic for Town05 given Traffic assignment zones and OD matrix
+#This script requires Sumo tools od2trips and duarouter and sumo tools can installed  using the command:  sudo apt-get install sumo-tools (https://sumo.dlr.de/docs/Installing/index.html)
+
 od2trips -c od2trips.config.xml -n Taz.xml --od-matrix-files OD_file.od -o od_file.odtrips.xml
 
 duarouter -c dua_router.cfg
+
+#removing extra route files
+
 rm od_file.odtrips.xml Town05.rou.alt.xml
 
+# Carla Passenger vehicle classes to add carla vehicle types in Sumo routes
 
-
-# List of possible vehicle types
 vehicle_classes=(
   "vehicle.audi.a2"
   "vehicle.audi.tt"
@@ -22,13 +29,13 @@ vehicle_classes=(
   "vehicle.nissan.micra"
 )
 
-input_file="Town05.rou.xml"  # Change this to your actual file name
+input_file="Town05.rou.xml"  
 temp_file="temp.xml"
 
-# Process the file line by line
+# Adding carla vehicle types in routes
 while IFS= read -r line; do
   if [[ "$line" == *'<vehicle '* ]]; then
-    # Select a random vehicle type
+    # Select a random vehicle type from carla vehicle class
     random_vehicle=${vehicle_classes[$RANDOM % ${#vehicle_classes[@]}]}
     
     # Add type attribute before the closing '>'
@@ -40,6 +47,6 @@ done < "$input_file"
 # Replace the original file with the updated file
 mv "$temp_file" "$input_file"
 
-echo "Background route file Town05.rou.xml has been sucssfully generated"
+echo "Background route file Town05.rou.xml has been successfully generated"
 
 
