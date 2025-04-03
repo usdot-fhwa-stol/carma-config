67928ca5953be140480012eb7f5b6a7efb1b429d
carma <Anishdeva17@gmail.com>
2025-04-03 17:01:49 -0400

    latest bp params

--- chrysler_pacifica_ehybrid_s_2019/drivers.launch.py
+++ chrysler_pacifica_ehybrid_s_2019/drivers.launch.py
@@ -50,7 +50,7 @@ def generate_launch_description():
 
     drivers = LaunchConfiguration('drivers')
     declare_drivers_arg = DeclareLaunchArgument(
-        name = 'drivers', default_value = 'v2x_ros_driver velodyne_lidar_driver_wrapper', description = "Desired drivers to launch specified by package name."
+        name = 'drivers', default_value = 'dsrc_driver v2x_ros_driver velodyne_lidar_driver_wrapper', description = "Desired drivers to launch specified by package name."
     )
 
     # Launch shutdown node which will ensure the launch file gets closed on system shutdown even if in a separate container
@@ -75,6 +75,18 @@ def generate_launch_description():
                 PythonLaunchDescriptionSource([ FindPackageShare('v2x_ros_driver'), '/launch/v2x_ros_driver.launch.py']),
                 launch_arguments = {
                     'log_level' : GetLogLevel('v2x_ros_driver', env_log_levels),
+                    }.items()
+            ),
+        ]
+    )
+    dsrc_group = GroupAction(
+        condition=IfCondition(PythonExpression(["'dsrc_driver' in '", drivers, "'.split()"])),
+        actions=[
+            PushRosNamespace(EnvironmentVariable('CARMA_INTR_NS', default_value='hardware_interface')),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([ FindPackageShare('dsrc_driver'), '/launch/dsrc_driver.py']),
+                launch_arguments = { 
+                    'log_level' : GetLogLevel('dsrc_driver', env_log_levels),
                     }.items()
             ),
         ]
@@ -130,6 +142,7 @@ def generate_launch_description():
         declare_vehicle_config_dir_arg,
         driver_shutdown_group,
         v2x_driver_group,
+        dsrc_group,
         lidar_group,
         gnss_ins_group,
         lightbar_driver_group
